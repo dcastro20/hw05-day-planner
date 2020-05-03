383dd61b2bfa326279916ca98ed5201e10452ff1
Doris Castro <dorisgeo12@gmail.com>
2020-05-02 23:53:21 -0400

    uploading homework

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hw05-third-party-api
+
+tried to make the calendar could not get the colors to attach.
---
+++ script.js
@@ -0,0 +1,18 @@
+// add the day 
+
+var a = moment(). format("MMMM Do");
+ 
+$("#currentDay").append(a);
+
+var timeStamp= moment().startOf("day").fromNow();
+console.log(timeStamp);
+
+var textArea = $("textarea");
+console.log(textArea)
+
+function hourStyle(){
+for( i=0; i < textArea.length;i++);
+
+
+}
+
